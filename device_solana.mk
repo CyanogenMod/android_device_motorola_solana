e70d74202c264a1fdd42b70c2c8359ae77a25e6e
Hashcode <hashcode0f@gmail.com>
2012-05-07 03:06:07 -0700

    update wifitether app
    Change-Id: I113f6a5f018567b36b78d1309e01bd814b6cf1e9

--- device_solana.mk
+++ device_solana.mk
@@ -136,7 +136,7 @@ PRODUCT_PACKAGES += \
 
 # WirelessTether
 PRODUCT_COPY_FILES += \
-    device/motorola/solana/prebuilt/app/wifi_tether_v3_1-beta11.apk:system/app/wifi_tether_v3_1-beta11.apk \
+    device/motorola/solana/prebuilt/app/wifi_tether_v3_1-beta14.apk:system/app/wifi_tether_v3_1-beta14.apk \
     device/motorola/solana/prebuilt/lib/libwtnativetask.so:system/lib/libwtnativetask.so \
 
 
