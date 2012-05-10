b1aba5ad63265dd46e524ae9de9b4d7e6171b402
David Hacker <davidhackerdvm@gmail.com>
2012-05-09 19:16:42 -0500

    add finalize release to fix bootstrap and permissions for init.d

--- releasetools/solana_edify_generator.py
+++ releasetools/solana_edify_generator.py
@@ -116,6 +116,11 @@ class EdifyGenerator(object):
     self.script.append('set_perm(0, 0, 0777, "/tmp/modelid_cfg.sh");')
     self.script.append(('run_program("/tmp/modelid_cfg.sh", "%s");' % command))
 
+  def RunFinalReleaseUtils(self):
+    self.script.append('package_extract_file("system/etc/releaseutils/finalize_release", "/tmp/finalize_release");')
+    self.script.append('set_perm(0, 0, 0777, "/tmp/finalize_release");')
+    self.script.append('run_program("/tmp/finalize_release");')
+
   def ShowProgress(self, frac, dur):
     """Update the progress bar, advancing it over 'frac' over the next
     'dur' seconds.  'dur' may be zero to advance it via SetProgress
