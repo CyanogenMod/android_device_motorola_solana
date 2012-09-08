c4e7c0fb7c98affcd05eb2d3229de5fecbc96264
David Hacker <davidhackerdvm@gmail.com>
2012-09-07 22:38:20 -0500

    CM10 board cleanup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,4 @@
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_solana.mk\
-    $(LOCAL_DIR)/aokp.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/full_solana.mk
+ifeq ($(TARGET_PRODUCT),aokp_solana)
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/aokp.mk
+endif
